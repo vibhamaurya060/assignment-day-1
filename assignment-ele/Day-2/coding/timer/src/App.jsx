b66a4cbea6445a2eedba3375e16208b9045b59b6
Vibha Maurya <vibhamaurya060@gmail.com>
2025-04-09 07:22:30 +0530

    add

--- assignment-ele/Day-2/coding/timer/src/App.jsx
+++ assignment-ele/Day-2/coding/timer/src/App.jsx
@@ -1,28 +1,33 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
    const [timer, setTimer]=useState(0);
-
-  useEffect(()=>{
-    let timerId = setInterval(()=>{
-      handleStart() 
-    },1000) 
-  },[])
+   const intervalRef=useRef(null);
 
    const handleStart=()=>{
-    setTimer((prev)=>prev+1);
-   }
+    if(intervalRef.current) return;
+    intervalRef.current=setInterval(()=>{
+      setTimer((prev)=>prev+1);
+    }, 1000);
+   };
 
    const handlePause=()=>{
-    setTimer((prev)=>prev);
-   }
+    clearInterval(intervalRef.current);
+    intervalRef.current=null;
+   } 
 
    const handleReset=()=>{
-    setTimer((prev)=>prev=0);
-   }
+    clearInterval(intervalRef.current);
+    intervalRef.current=null;
+    setTimer(0);
+   };
  
+   useEffect(()=>{
+    return ()=> clearInterval(intervalRef.currtent);
+   },[]);
+
   return (
     <>
       <h2>Timer: {timer}</h2>
